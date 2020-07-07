57881c88b7e93004987cd27d74f48b4783512fd5
naeline <mouloud.mala@gmail.com>
2020-07-07 11:56:20 +0200

    log

---
+++ src/DataFixtures/UtilisateurFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Utilisateur;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UtilisateurFixtures extends Fixture
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $utilisateur = new Utilisateur();
+        $utilisateur->setUsername('demo');
+        $utilisateur->setPassword($this->encoder->encodePassword($utilisateur, 'demo'));
+        $manager->persist($utilisateur);
+        $manager->flush();
+    }
+}
